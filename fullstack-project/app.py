3ded73c0f0c9faa75336d57b88f53159ec54ab4f
Saqib <saqibmails@gmail.com>
2022-04-08 12:45:20 +0100

    update

--- fullstack-project/app.py
+++ fullstack-project/app.py
@@ -8,14 +8,33 @@ app= Flask(__name__)
 def home():
     return render_template("index.html")
 
-    @app.errohandler(404)
-    def page_not_found (e):
-        return render_template("404.html"),
-        404
-    @app.errorhandler(500)
-    def internal_server_erros(e):
-        return render_template("500.html"),
-        500
+@app.route("/aboutus")
+def aboutus():
+    return render_template("aboutus.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/bookings")
+def bookings():
+    return render_template("bookings.html")
+@app.route("/contactus")
+def contactus():
+    return render_template("contactus.html")
+
+
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"),500
+
+
 #############################################################
 
 
